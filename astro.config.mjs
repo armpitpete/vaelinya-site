907d794501de6cb7a5080f23b836010cdbb3f536
Merrin W. Dream <post@rimmerworld.info>
2026-06-01 21:48:00 +0100

    Wire public story cleanup and lexicon redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import removePublicStoryScaffolding from './src/plugins/removePublicStoryScaffolding.mjs';
+
+const lexiconEntryRedirects = Object.fromEntries(
+  'abcdefghijklmnopqrstuvwxyz'.split('').flatMap((letter) => [
+    [`/lexicon/entries/${letter}`, '/language/lexicon/'],
+    [`/lexicon/entries/${letter}/`, '/language/lexicon/'],
+  ])
+);
 
 export default defineConfig({
   site: 'https://vaelinya.uk',
   integrations: [sitemap()],
+  markdown: {
+    remarkPlugins: [removePublicStoryScaffolding],
+  },
   redirects: {
     '/stories': '/read/',
     '/stories/': '/read/',
@@ -11,8 +22,9 @@ export default defineConfig({
     '/stories/lina/': '/read/lina/',
     '/lexicon': '/language/lexicon/',
     '/lexicon/': '/language/lexicon/',
-    '/lexicon/entries/s': '/language/lexicon/',
-    '/lexicon/entries/s/': '/language/lexicon/',
+    '/lexicon/entries': '/language/lexicon/',
+    '/lexicon/entries/': '/language/lexicon/',
+    ...lexiconEntryRedirects,
     '/encyclopedia': '/world/',
     '/encyclopedia/': '/world/',
     '/encyclopedia/world': '/world/',
